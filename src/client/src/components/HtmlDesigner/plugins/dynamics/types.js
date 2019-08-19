ab9ba360963fe6edb978b11983bccc857385887d
Ryan Spaeth <rspaeth@mvqn.net>
2019-08-19 07:10:22 -0700

    - Backup! - Mostly Working!

--- src/client/src/components/HtmlDesigner/plugins/dynamics/types.js
+++ src/client/src/components/HtmlDesigner/plugins/dynamics/types.js
@@ -1,62 +1,68 @@
-import { dynamicField } from "./consts";
-import { inlineFromCSS } from "./helpers";
-
-
-export default function(editor, opt = {})
+/**
+ * Export the plugin's "types" function.
+ * @param {Object} editor
+ * @param {Object} options
+ */
+export default function(editor, options = {})
 {
+    // Get a quick reference to the default Component, for later use.
     const defaultType = editor.DomComponents.getType("default");
 
     /**
-     *
-     * @param {Object} styleObj
+     * Converts a style Object to it's inline style string representation.
+     * @param {Object} style
      * @returns {string}
      */
-    const styleObjectToString = function(styleObj)
+    const styleObjectToString = function(style)
     {
         let styles = [];
 
-        Object.keys(styleObj).forEach(function(key)
+        Object.keys(style).forEach(function(key)
         {
-            styles.push(`${key}:${styleObj[key]}`);
+            styles.push(`${key}:${style[key]}`);
         });
 
         return styles.join(";");
     };
 
-
-
-    // Define a component with `textable` property...
+    // Add our new type.
     editor.DomComponents.addType("dynamic-field",
     {
-
-
-
+        /**
+         * Used in determining whether or not the given component is handled by our type.
+         * @param value
+         * @returns {*}
+         */
         isType: function(value)
         {
             if(value && value.type === "dynamic-field")
                 return value;
-        },
 
+        },  // isType()
 
         // Setup the component's model.
-        model:  {
-
-            // Set the model defaults.
-            defaults: {
-
-                // Is allowed to be dropped inside a text component.
+        model:
+        {
+            // Specifically, the model defaults.
+            defaults:
+            {
+                // This component is allowed to be dropped inside of a text component.
                 textable: true,
 
-                // Can not have anything else dropped inside of itself.
+                // But can not have anything else dropped inside of itself.
                 droppable: false,
 
-                style: {
+                // Set some default styles.
+                style:
+                {
                     display: "inline-block",
-                },
+
+                },  // styles
 
                 // Define the component's traits.
-                traits: [
-                    {
+                traits:
+                [
+                    {   // The object from which to get our data.
                         type: "select",
                         label: "Object",
                         name: "field-object",
@@ -67,7 +73,8 @@ export default function(editor, opt = {})
                             "job",
                         ],
                     },
-                    {
+
+                    {   // The property on that object from which to get our data.
                         type: "select",
                         label: "Property",
                         name: "field-property",
@@ -77,104 +84,122 @@ export default function(editor, opt = {})
                         ],
                     },
 
+                    {   // An optional default value to display if the above is null, defaults to empty.
+                        type: "text",
+                        label: "Default",
+                        name: "field-default",
+                        placeholder: "Value if null..."
+                    },
+
+                    {   // An optional condition for determining the visibility of the field, defaults to empty.
+                        type: "text",
+                        label: "Condition",
+                        name: "field-condition",
+                        placeholder: "Condition for rendering..."
+                    },
 
 
-                ],
+                ],  // traits
 
                 // Set default values for the same traits.
-                attributes: {
+                attributes:
+                {
                     "field-object": "user",
                     "field-property": "firstName",
-                },
+                    "field-default": "",
+                    "field-condition": "",
 
+                },  // attributes
 
-
-
-
-            },
+            },  // defaults
 
 
             /**
-             *
+             * Renders our component as HTML.
              * @returns {string}
              */
             toHTML: function()
             {
-
+                // Get the component's attributes and styles.
                 const attributes = this.getAttributes();
                 const styles = styleObjectToString(this.getStyle());
 
+                const content =
+                    `${attributes["field-object"]}.${attributes["field-property"]}` + (
+                    attributes["field-default"] === ""
+                        ? ""
+                        : `|default('${attributes["field-default"]}')`
+                );
+
+                const conditionOpen = (
+                    attributes["field-condition"] === ""
+                        ? ""
+                        : `{% if ${attributes["field-condition"]} %}`
+                );
+                const conditionClose = (
+                    attributes["field-condition"] === ""
+                        ? ""
+                        : `{% endif %}`
+                );
+
+                // And return the combination in our desired HTML format!
                 return `
+                    ${conditionOpen}
                     <div style="${styles}">
-                        {% ${attributes["field-object"]}.${attributes["field-property"]} %}
+                        {{ ${content} }}
                     </div>
+                    ${conditionClose}
                 `;
-            },
 
+            },  // toHTML()
 
+        },  // model
 
-        },
-
-
-        // The view below it's just an example of creating a different UX
-        view: /* editor.DomComponents.getType("default").view.extend( */
+        // The view differs from the actual HTML content, so we need render it manually...
+        view:
         {
+            // The visual element should be surrounded with a <div>.
             tagName: "div",
 
+            /**
+             * Initialize the component's view.
+             */
             init: function()
             {
-                let self = this;
+                this.listenTo(this.model, "change:attributes", () => { this.render(); });
+                this.listenTo(this.model, "change:style", () => { this.render(); });
 
-                /*
+            },  // init()
 
-                // Get the current model styles, likely none at the moment!
-                let style = this.model.getStyle();
-
-                // Merge the "common" styles with the current model styles.
-                style = Object.assign({ display: "inline-block" }, style);
-
-                // Assign the combined styles to the model.
-                this.model.setStyle(style);
-
-                */
-
-                this.listenTo(this.model, "change:attributes", function()
-                {
-                    //console.log("attribute changed!");
-                });
-
-                this.listenTo(this.model, "change:style", function(model, style)
-                {
-                    self.render();
-                    //this.$el.css("background-color", "lightblue");
-                })
-            },
-
+            /**
+             * Render the component's view.
+             * @returns {view}
+             */
             render: function ()
             {
-                // Apply the defaults!
+                // Execute the default render() function...
                 defaultType.view.prototype.render.apply(this, arguments);
 
-                const {model, el} = this;
+                // Get the component's attributes.
+                const attributes = this.model.getAttributes();
 
-                const attributes = model.getAttributes();
+                // Then specifically the ones we need for displaying information to the user.
                 const fieldObject = attributes["field-object"];
                 const fieldProperty = attributes["field-property"];
 
-                const $el = $(el);
-                $el.html(`${fieldObject}.${fieldProperty}`);
-                $el.css("background-color", "lightblue");
+                // Set the visual content in the editor only.
+                this.$el.html(`${fieldObject}.${fieldProperty}`);
+
+                // And add any editor only styles.
+                this.$el.css("background-color", "lightblue");
                 //$el.css("border-radius", "0.25rem");
 
-
-
-
-
+                // Finally, we need to return the view!
                 return this;
 
-            },
+            },  // render()
 
-        } //)
+        }   // view
 
     });
 
